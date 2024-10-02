25bfb5181586272d8705ed7283f021ab6df1d53f
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2024-10-02 17:25:33 +0530

    Enhancements Overview
    This project features key improvements to the Home and Navbar components for a better user experience:
    
    Home Component:
    Animation Effects: Added entrance animations using the framer-motion library for icons, headings, and cards.
    Card Interactivity: Enhanced hover effects for cards, including transformations and color changes.
    Responsive Layout: Maintained centered elements for a balanced appearance across devices.
    Navbar Component:
    Responsive Design: Optimized for various screen sizes with stacked elements on smaller screens.

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+
+    apiKey: "your-api-key",
+    authDomain: "your-auth-domain",
+    projectId: "your-project-id",
+    storageBucket: "your-storage-bucket",
+    messagingSenderId: "your-messaging-sender-id",
+    appId: "your-app-id",
+
+
 };
 
 const app = initializeApp(firebaseConfig);
