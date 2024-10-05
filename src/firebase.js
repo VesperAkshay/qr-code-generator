e7ba5488866b7b685a48c4b85b4549af1269564d
AdityaP700 <126982848+AdityaP700@users.noreply.github.com>
2024-10-05 13:54:11 +0530

    feat: Implement minimum character limit

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
+      apiKey: "AIzaSyAeokJU87mygt98ssZK2bI90fXhDQuqkjU",
+      authDomain: "ciphersafe-b94a5.firebaseapp.com",
+      projectId: "ciphersafe-b94a5",
+      storageBucket: "ciphersafe-b94a5.appspot.com",
+      messagingSenderId: "173234055063",
+      appId: "1:173234055063:web:5268b279581bd5e87246d2",
+      measurementId: "G-X3561QHDSC"
+    };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -19,3 +19,26 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 export { auth, db, storage };
+
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAeokJU87mygt98ssZK2bI90fXhDQuqkjU",
+//   authDomain: "ciphersafe-b94a5.firebaseapp.com",
+//   projectId: "ciphersafe-b94a5",
+//   storageBucket: "ciphersafe-b94a5.appspot.com",
+//   messagingSenderId: "173234055063",
+//   appId: "1:173234055063:web:5268b279581bd5e87246d2",
+//   measurementId: "G-X3561QHDSC"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
