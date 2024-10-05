671dddce57dc45eeb00ffd425ba1755d8ee5ce1d
Anuj <anujshrivastav530@gmail.com>
2024-10-05 15:54:30 +0530

    modified unnecesary files

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo512.png"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>My Next.js App</title>
       </Head>
